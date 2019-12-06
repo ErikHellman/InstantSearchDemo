67b27aab288d800c597e1e2f05214b2e8bb775a2
Erik Hellman <erik.hellman@hellsoft.se>
2019-12-06 14:27:29 +0100

    Clear up things

--- app/src/test/java/se/hellsoft/android/instantsearchdemo/SearchViewModelTest.kt
+++ app/src/test/java/se/hellsoft/android/instantsearchdemo/SearchViewModelTest.kt
@@ -3,10 +3,7 @@ package se.hellsoft.android.instantsearchdemo
 import android.content.res.AssetManager
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.runBlocking
-import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.mockito.ArgumentMatchers.anyString
 
@@ -20,19 +17,8 @@ class SearchViewModelTest {
         val searchRepository = SearchRepository(assets)
         val viewModel = SearchViewModel(searchRepository)
 
-        viewModel.internalSearchResult.collect {
-            when(it) {
-                is ValidResult -> assertEquals(1, it.result.size)
-            }
-        }
+        // TODO Write test..
 
-        viewModel.search("Antitheses")
-
-
-        delay(1000)
-
-        viewModel.queryChannel.close()
-
-        return@runBlocking 
+        return@runBlocking
     }
 }
